f0b8cc8088a0e8ca531b85eaf6edbe72049f50a4
Magnolia <magnolia1925pa@gmail.com>
2020-01-26 14:14:32 +0900

    add user resource and first action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  # 暗黙の了解でshow=/user/1対応
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+  resources :users
 end
