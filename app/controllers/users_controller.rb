39a048723148d8202295cb8330c97f2cd02c94db
Magnolia <magnolia1925pa@gmail.com>
2020-01-29 01:32:09 +0900

    add users controller comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,12 @@ class UsersController < ApplicationController
   end
 
   def new
+    # POST処理に必要なインスタンス変数を生成
     @user = User.new
   end
 
   def create
+    # newアクションで生成したインスタンス変数からパラメータを受け取ってインスタンス変数を生成
     # セキュリティ上、一度privateメソッドを通してから保存する
     @user = User.new(user_params)
     if @user.save
