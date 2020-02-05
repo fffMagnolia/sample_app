1f7300d5fe06b5cc168e2100ea6204765f6c4b0c
Magnolia <magnolia1925pa@gmail.com>
2020-02-06 00:45:30 +0900

    include SessionHelper in Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello world!"
-  end
+  # 全てのコントローラからSessionHelperが使用可能になる
+  include SessionHelper
 end
