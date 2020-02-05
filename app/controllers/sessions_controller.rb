f076c6ce9539353d0671d62542a8ce19821409f5
Magnolia <magnolia1925pa@gmail.com>
2020-02-05 13:02:31 +0900

    add create action sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
+  # ログインページにアクセスした時のアクション
   def new
+  end
+
+  # わかりにくいがログイン情報を送信した時のアクション
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    # authenticate: has_secure_passwordを追加することにより提供されるメソッド
+    if user && user.authenticate(params[:session][:password])
+      # do something
+    else
+      flash.now[:danger] = 'Invalid email or password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
   end
 end
