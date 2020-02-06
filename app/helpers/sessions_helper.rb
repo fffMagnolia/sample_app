a8e097771a79bb4f47b51185f571e121a5d845a8
Magnolia <magnolia1925pa@gmail.com>
2020-02-07 00:10:15 +0900

    update sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,10 @@ module SessionsHelper
     # session: Railsの組み込みメソッド
     session[:user_id] = user.id
   end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
