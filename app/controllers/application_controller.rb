4c44939228470d650920cc6bfa66fe989296c517
Magnolia <magnolia1925pa@gmail.com>
2020-02-08 12:58:44 +0900

    fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # 全てのコントローラからSessionHelperが使用可能になる
-  include SessionHelper
+  # 全てのコントローラからSessionsHelperが使用可能になる
+  include SessionsHelper
 end
