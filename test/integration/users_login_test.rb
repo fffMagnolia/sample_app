9ff76038f7267623ffffeb26e86b6b2b073b084b
Magnolia <magnolia1925pa@gmail.com>
2020-02-22 00:37:07 +0900

    update users login test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    # users.ymlからfixtureを呼び出す
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -12,5 +17,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     # フラッシュメッセージが消えていることを期待
     assert flash.empty?
+  end
+
+  test "login with valid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    # ユーザの個別ページにリダイレクトすることを期待
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0 
+    assert_select "a[href=?]", logout_path 
+    assert_select "a[href=?]", user_path(@user)
+
   end
 end
