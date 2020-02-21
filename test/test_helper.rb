7cc25abc04e578303f3e6886f7684d1e4c84ce37
Magnolia <magnolia1925pa@gmail.com>
2020-02-22 01:00:22 +0900

    update test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,7 @@
+# =====================================
+# controller側のヘルパーをテストで呼び出すことはできないので、
+# ここに再定義する
+# =====================================
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
@@ -8,5 +12,8 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  # sessions helperのlogged_in?と同じ動作
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
